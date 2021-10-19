195480a195ba1ef66abf39a1219a3c448731bc72
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-10-19 13:30:13 -0400

    Added more HTML structure and JS call.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,14 @@ var upcomingForecastEl = document.getElementById('upcoming-forecast');
 
 // defining primary function
 function weatherSearch() {
-    console.log(searchInputEl.value);
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInputEl.value + '&units=imperial&appid=2aa854b2e1b53a068dd2a8b6738c490f';
+
+    $.ajax({
+        url: weatherUrl,
+        method: 'GET',
+    }).then(function (response) {
+        console.log(response);
+    })
 }
 
 // event listener for when search button is pressed
